9fe12158812d43747b2332ae0f42e6ff2888372a
Rog A. de Souza <pixelrogueart@gmail.com>
2025-03-03 15:19:26 -0300

    Dynamic Goap

--- client/goap-godot/src/entities/storage/storage.gd
+++ client/goap-godot/src/entities/storage/storage.gd
@@ -3,14 +3,14 @@ extends Node2D
 
 
 var wood_amount = 0
-var storage_space = 10
+var storage_space = 3
 
 
 func insert_wood():
 	wood_amount += 1
 	update_state()
 	print("Inserted Wood! Wood amount: %s"%wood_amount)
-	return storage_space < wood_amount
+	return storage_space <= wood_amount
 
 
 func update_state() -> void: 
