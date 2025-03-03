ea91690542d2c16a738e1775d96898185bf71eaf
Rog A. de Souza <pixelrogueart@gmail.com>
2025-03-03 16:52:23 -0300

    Sleep & Arrive state

---
+++ client/goap-godot/src/entities/pawn/goals/arrive_at_location.gd
@@ -0,0 +1,13 @@
+class_name ArriveAtLocationGoal
+extends GoapGoal
+
+func is_valid() -> bool:
+	return _actor.world_node.is_at_grid_position(_actor, _world_state._state["target_position"])
+
+
+func get_priority() -> int:
+	return priority
+
+
+func get_desired_state() -> Dictionary:
+	return desired_state
